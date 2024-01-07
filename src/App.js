bbf3c0be908da270becca889778b119b1f012a2d
Von Ezekiel Manaois <von.manaois@gmail.com>
2024-01-06 20:38:49 -0500

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,166 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddTask(task) {
+    setTasks((tasks) => [...tasks, task]);
+  }
+
+  function handleDoneTask(id) {
+    setTasks((tasks) =>
+      tasks.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  }
+
+  function handleClearTasks() {
+    setTasks([]);
+  }
+
+  function handleDeleteTask(id) {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app-container">
+        <Form tasks={tasks} onAddTask={handleAddTask} />
+        <TaskList
+          tasks={tasks}
+          onTaskDone={handleDoneTask}
+          onClearTasks={handleClearTasks}
+          onDeleteTask={handleDeleteTask}
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Form({ tasks, onAddTask }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !description) return;
+    const newTask = {
+      id: Date.now(),
+      name,
+      description,
+      priority,
+      done: false,
+    };
+    console.log(newTask);
+    onAddTask(newTask);
+  }
+  return (
+    <div className="form-container">
+      <h2>Add Task</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Task Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label>Priority</label>
+        <select
+          value={priority}
+          onChange={(e) => setPriority(Number(e.target.value))}
+        >
+          <option value={3}>Low</option>
+          <option value={2}>Medium</option>
+          <option value={1}>High</option>
+        </select>
+        <button>Add</button>
+      </form>
+
+      <Status tasks={tasks} />
+    </div>
+  );
+}
+
+function Task({ task, onTaskDone, onDeleteTask }) {
+  const handleDoneTask = () => {
+    onTaskDone(task.id);
+  };
+
+  function handleDeleteTask() {
+    onDeleteTask(task.id);
+  }
+  let priority = task.priority;
+  if (task.priority === 1) priority = "High";
+  if (task.priority === 2) priority = "Medium";
+  if (task.priority === 3) priority = "Low";
+  return (
+    <div className={!task.done ? "task" : "task-done"}>
+      <h3>{task.name}</h3>
+      <p>{task.description}</p>
+      <p>Priority: {priority}</p>
+      <button onClick={handleDoneTask}>
+        {!task.done ? "Done" : "Not Done"}
+      </button>
+      <button onClick={handleDeleteTask}>Delete</button>
+    </div>
+  );
+}
+
+function TaskList({ tasks, onTaskDone, onClearTasks, onDeleteTask }) {
+  const [sortBy, setSortBy] = useState("priority");
+
+  let sortedTasks;
+
+  function handleClearTasks() {
+    onClearTasks();
+  }
+
+  if (sortBy === "input") sortedTasks = tasks;
+  if (sortBy === "description")
+    sortedTasks = tasks.slice().sort((a, b) => a.name.localeCompare(b.name));
+  if (sortBy === "status")
+    sortedTasks = tasks.slice().sort((a, b) => a.done - b.done);
+  if (sortBy === "priority")
+    sortedTasks = tasks.slice().sort((a, b) => a.priority - b.priority);
+
+  return (
+    <div className="task-container">
+      <div className="filter">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by name order</option>
+          <option value="status">Sort by status</option>
+          <option value="priority">Sort by priority</option>
+        </select>
+        <button onClick={handleClearTasks}>Clear Tasks</button>
+      </div>
+      <div className="task-list">
+        {sortedTasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onTaskDone={onTaskDone}
+            onDeleteTask={onDeleteTask}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Status({ tasks }) {
+  const numtasks = tasks.filter((task) => !task.done).length;
+  return (
+    <footer>
+      <h3>You have {numtasks} tasks.</h3>
+    </footer>
+  );
+}
