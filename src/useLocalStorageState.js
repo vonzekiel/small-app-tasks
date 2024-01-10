29c92d5f214cfdbb4313b3e5ea18150eac664330
Von Ezekiel Manaois <von.manaois@gmail.com>
2024-01-10 12:13:41 -0500

    .

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
-    console.log(storedValue);
     return storedValue ? JSON.parse(storedValue) : initialState;
   });
 
